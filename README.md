68effec2883b49511b157e70e39502d7f551aeab
kgunase1 <kgunase1@binghamton.edu>
2023-04-27 00:39:20 -0400

    task 3 finished

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ Analysis that finds tracks with similar audio features based on multiple criteri
 In this example, we're filtering for tracks with a danceability score greater than or equal to 0.7, an energy score greater than or equal to 0.8, a tempo greater than or equal to 130, and a minimum of 1 million views on YouTube. We're also calculating the ratio of likes to views and sorting the results in descending order by this ratio. Finally, we're limiting the results to the top 10 most popular tracks that meet the specified criteria.
 
 The output includes information about each track, including the track name, artist, danceability score, energy score, tempo, number of views, number of likes, like/view ratio, and title of the corresponding YouTube video. You can adjust the filtering and sorting criteria to match your specific needs.
+
+Task 3:
+Analyze the relationship between video attributes and video popularity: You can analyze the relationship between video attributes (such as licensed, official video, and live stream) and video popularity (based on views, likes, and comments on YouTube) to identify which attributes are most important for a music video's success and popularity.
---
+++ task3.py
@@ -0,0 +1,77 @@
+import pymongo
+import matplotlib.pyplot as plt
+
+# Connect to MongoDB server
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+
+db = client['local']
+collection = db['Spotify_Youtube']
+
+# Define the aggregation pipeline
+pipeline = [
+   {
+      "$match": { "Views": { "$gt": 1000000 } }
+   },
+   {
+      "$group":
+      {
+         "_id": { "licensed": "$Licensed", "official_video": "$official_video" },
+         "total_views": { "$sum": "$Views" },
+         "total_likes": { "$sum": "$Likes" },
+         "total_comments": { "$sum": "$Comments" },
+         "count": { "$sum": 1 }
+      }
+   },
+   {
+      "$project":
+      {
+         "licensed": "$_id.licensed",
+         "official_video": "$_id.official_video",
+         "total_views": 1,
+         "total_likes": 1,
+         "total_comments": 1,
+         "count": 1,
+         "avg_views": { "$divide": ["$total_views", "$count"] },
+         "avg_likes": { "$divide": ["$total_likes", "$count"] },
+         "avg_comments": { "$divide": ["$total_comments", "$count"] }
+      }
+   },
+   {
+      "$sort":
+      {
+         "avg_views": -1
+      }
+   }
+]
+
+# Execute the aggregation pipeline and extract the results
+results = list(collection.aggregate(pipeline))
+
+# Create lists for each field
+labels = ["{} - {}".format(result["_id"]["licensed"], result["_id"]["official_video"]) for result in results]
+views = [result["avg_views"] for result in results]
+likes = [result["avg_likes"] for result in results]
+comments = [result["avg_comments"] for result in results]
+
+# # Create a bar chart using matplotlib
+# plt.bar(labels, views)
+
+# # Add axis labels and a title to the chart
+# plt.xlabel("Licensed / Official Video")
+# plt.ylabel("Average Views")
+# plt.title("Average Views by Video Attributes")
+
+# # Display the chart
+# plt.show()
+
+# Plot the results
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.bar(labels, views, color="b", label="Average Views")
+ax.bar(labels, likes, color="g", label="Average Likes")
+ax.bar(labels, comments, color="r", label="Average Comments")
+ax.set_title("Relationship between Video Attributes and Popularity")
+ax.set_xlabel("Licensed - Official Video")
+ax.set_ylabel("Average Value")
+ax.legend()
+plt.xticks(rotation=45)
+plt.show()
